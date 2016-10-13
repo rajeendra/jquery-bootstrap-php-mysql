f3c85bb53933013722c3f3b4af6eeeed0899c218
rajeendra <rajeendra.kanishka@gmail.com>
2016-10-13 17:52:51 +0530

    Sec commit

--- form-handler.php
+++ form-handler.php
@@ -3,9 +3,22 @@
 
 // Test for db
 // $db = new mysqli("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME");
+
+
+//openShift
+//define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+//define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+//define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+//define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+//define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+//$db = new mysqli(DB_HOST,"adminva3m596","jfuSmfJND_4Y","php");
+
+// local
 $db = new mysqli("localhost","root","root","test");
+
 //$message = "wrong answer";
 //echo "<script type='text/javascript'>alert('$message');</script>";
+
 $sql = "SELECT id FROM form_submissions";
 $result = $db->query($sql);
 if (empty($result)) {
